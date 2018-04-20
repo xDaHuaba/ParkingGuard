37c41f49277aee7c1df194b593c136d21e803ec2
Benjamin <saxoben34@gmail.com>
2018-04-20 15:08:46 +0200

    NewEditWindow

---
+++ Adminsoftware/src/at/htlwels/it/gruber/view/NewEditController.java
@@ -0,0 +1,16 @@
+package at.htlwels.it.gruber.view;
+
+import javafx.scene.control.Button;
+import javafx.scene.control.DatePicker;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+
+public class NewEditController {
+    public Button speichern;
+    public Button abbrechen;
+    public TextField kennzeichenfield;
+    public TextField vornamenfield;
+    public TextField nachnamefield;
+    public DatePicker parktvonpick;
+    public DatePicker parktbispick;
+}
